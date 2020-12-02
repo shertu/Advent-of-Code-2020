4926a22bd89751479c22de634cb188ba40bdcae8
shertu <djared.xeknau@outlook.com>
2020-12-02 23:35:34 +1100

    D1 solution

--- 1/main.go
+++ 1/main.go
@@ -1,55 +1,92 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "os"
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+  "sort"
 )
 
-func check(e error) {
-    if e != nil {
-        panic(e)
+func ReadInput(filename string) ([]int, error) {
+	f, err := os.Open(filename) // (*File, error)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ReadSplitIntegers(f)
+}
+
+func ReadSplitIntegers(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result []int
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+	return result, scanner.Err()
+}
+
+func FindComponents(input []int, componentSumTarget int, componentToFindCount int) []int {
+	if componentToFindCount < 0 {
+		panic("The number of components must be positive.")
+	}
+
+  if componentToFindCount > len(input) {
+    return nil
+  }
+
+  if componentToFindCount == 0 && componentSumTarget == 0 {
+    return []int { }
+  }
+
+  for i, expense := range input {
+    output := []int {expense}
+
+    // simple case
+    if componentToFindCount == 1 {
+      if expense == componentSumTarget {
+        return output
+      }
     }
+
+    // complex case
+    if componentToFindCount > 1 {
+      remainder := componentSumTarget - expense
+
+      // only ever need to search forward for values
+      for _, result := range FindComponents(input[i:], remainder, componentToFindCount - 1) {
+        output = append(output, result)
+      }
+
+      if len(output) >= componentToFindCount {
+        return output
+      }
+    }
+  }
+
+  return nil
 }
 
 func main() {
+	var filename string = "input.txt"
+	input, err := ReadInput(filename)
 
-    dat, err := ioutil.ReadFile("/tmp/dat")
-    check(err)
-    fmt.Print(string(dat))
+	if err != nil {
+		panic(err)
+	}
 
-    f, err := os.Open("/tmp/dat")
-    check(err)
+  // sort the input
+  sort.Ints(input)
 
-    b1 := make([]byte, 5)
-    n1, err := f.Read(b1)
-    check(err)
-    fmt.Printf("%d bytes: %s\n", n1, string(b1[:n1]))
+	// for each potential match
+	var alpha = FindComponents(input, 2020, 3)
 
-    o2, err := f.Seek(6, 0)
-    check(err)
-    b2 := make([]byte, 2)
-    n2, err := f.Read(b2)
-    check(err)
-    fmt.Printf("%d bytes @ %d: ", n2, o2)
-    fmt.Printf("%v\n", string(b2[:n2]))
-
-    o3, err := f.Seek(6, 0)
-    check(err)
-    b3 := make([]byte, 2)
-    n3, err := io.ReadAtLeast(f, b3, 2)
-    check(err)
-    fmt.Printf("%d bytes @ %d: %s\n", n3, o3, string(b3))
-
-    _, err = f.Seek(0, 0)
-    check(err)
-
-    r4 := bufio.NewReader(f)
-    b4, err := r4.Peek(5)
-    check(err)
-    fmt.Printf("5 bytes: %s\n", string(b4))
-
-    f.Close()
+	fmt.Println(alpha)
 }

e82e6784223293ad40acd53bead0fb40d27411ab
shertu <djared.xeknau@outlook.com>
2020-12-06 03:16:55 +1100

    Reduced day four line count to 120 LoC

--- 4/main.go
+++ 4/main.go
@@ -10,32 +10,20 @@ import (
 	"strings"
 )
 
-type Passport struct {
-	byr, iyr, eyr, hgt, hcl, ecl, pid, cid string
-}
-
-func ReadInput(r io.Reader) ([]map[string]string, error) {
+func ReadInput(r io.Reader) ([]string, error) {
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanLines)
 
-	var result []map[string]string
-	var passport = make(map[string]string, 0)
+	var result []string
+	var passport string
 
 	for scanner.Scan() {
 		// Check if there is an empty line to indicate a new passport.
 		if text := scanner.Text(); len(text) > 0 {
-			kvs, err := ReadInputLine(strings.NewReader(text))
-			if err != nil {
-				return result, err
-			}
-
-			// merge passport value maps
-			for k, v := range kvs {
-				passport[k] = v
-			}
+			passport += " " + text
 		} else {
 			result = append(result, passport)
-			passport = make(map[string]string, 0)
+			passport = ""
 		}
 	}
 
@@ -43,101 +31,59 @@ func ReadInput(r io.Reader) ([]map[string]string, error) {
 	return result, scanner.Err()
 }
 
-func ReadInputLine(r io.Reader) (map[string]string, error) {
+func ValidatePassport(passport string) (bool, error) {
+	r := strings.NewReader(passport)
 	scanner := bufio.NewScanner(r)
 	scanner.Split(bufio.ScanWords)
 
-	var result = make(map[string]string, 0)
+	var okCount int
+	var hasCid bool
 
 	for scanner.Scan() {
-		k, v, err := StringConvertPassportKeyValuePair(scanner.Text())
-		if err != nil {
-			return result, err
+		re := regexp.MustCompile(`(\w{3}):(.*)`)
+		text := scanner.Text()
+		matches := re.FindStringSubmatch(text)
+		if len(matches) > 0 {
+			var k, v = matches[1], matches[2]
+			var ok bool
+			switch k {
+			case "byr":
+				ok = ValidatePassportYear(v, 1920, 2002)
+			case "iyr":
+				ok = ValidatePassportYear(v, 2010, 2020)
+			case "eyr":
+				ok = ValidatePassportYear(v, 2020, 2030)
+			case "hgt":
+				ok = ValidatePassportHeight(v)
+			case "hcl":
+				ok, _ = regexp.MatchString(`#[[:xdigit:]]{6}`, v)
+			case "ecl":
+				ok, _ = regexp.MatchString(`amb|blu|brn|gry|grn|hzl|oth`, v)
+			case "pid":
+				ok, _ = regexp.MatchString(`\d{9}`, v)
+			case "cid":
+				ok = true
+				hasCid = true
+			}
+
+			//fmt.Println(text, ok)
+			if ok {
+				okCount++
+			} else {
+				break
+			}
 		}
-		result[k] = v
 	}
 
-	return result, scanner.Err()
+	//fmt.Println(passport, okCount)
+	if hasCid {
+		return okCount >= 8, scanner.Err()
+	} else {
+		return okCount >= 7, scanner.Err()
+	}
 }
 
-func StringConvertPassportKeyValuePair(s string) (string, string, error) {
-	re := regexp.MustCompile(`(\w{3}):(.*)`)
-	matches := re.FindStringSubmatch(s)
-	if len(matches) == 0 {
-		return "", "", fmt.Errorf("\"%v\" does not match the passport key value regex", s)
-	}
-	return matches[1], matches[2], nil
-}
-
-func ConvertMapToPassport(m map[string]string) (Passport, bool) {
-	var passport Passport
-
-	if val, ok := m["byr"]; ok {
-		passport.byr = val
-	} else {
-		return passport, false
-	}
-
-	if val, ok := m["iyr"]; ok {
-		passport.iyr = val
-	} else {
-		return passport, false
-	}
-
-	if val, ok := m["eyr"]; ok {
-		passport.eyr = val
-	} else {
-		return passport, false
-	}
-
-	if val, ok := m["hgt"]; ok {
-		passport.hgt = val
-	} else {
-		return passport, false
-	}
-
-	if val, ok := m["hcl"]; ok {
-		passport.hcl = val
-	} else {
-		return passport, false
-	}
-
-	if val, ok := m["ecl"]; ok {
-		passport.ecl = val
-	} else {
-		return passport, false
-	}
-
-	if val, ok := m["pid"]; ok {
-		passport.pid = val
-	} else {
-		return passport, false
-	}
-
-	if val, ok := m["cid"]; ok {
-		passport.cid = val
-	} else {
-		//return passport, false
-	}
-
-	return passport, true
-}
-
-func IsPassportValuesValid(p Passport) bool {
-	okHCL, _ := regexp.MatchString(`#[[:xdigit:]]{6}`, p.hcl)
-	okECL, _ := regexp.MatchString(`amb|blu|brn|gry|grn|hzl|oth`, p.ecl)
-	okPID, _ := regexp.MatchString(`\d{9}`, p.pid)
-
-	return IsPassportKVValidYear(p.byr, 1920, 2002) &&
-		IsPassportKVValidYear(p.iyr, 2010, 2020) &&
-		IsPassportKVValidYear(p.eyr, 2020, 2030) &&
-		IsPassportKVValidHeight(p.hgt) &&
-		okHCL &&
-		okECL &&
-		okPID
-}
-
-func IsPassportKVValidYear(v string, min int, max int) (matched bool) {
+func ValidatePassportYear(v string, min int, max int) (matched bool) {
 	re := regexp.MustCompile(`(\d{4})`)
 	matches := re.FindStringSubmatch(v)
 	if len(matches) == 0 {
@@ -147,7 +93,7 @@ func IsPassportKVValidYear(v string, min int, max int) (matched bool) {
 	return n >= min && n <= max
 }
 
-func IsPassportKVValidHeight(v string) (matched bool) {
+func ValidatePassportHeight(v string) (matched bool) {
 	re := regexp.MustCompile(`(\d+)(in|cm)`)
 	matches := re.FindStringSubmatch(v)
 	if len(matches) == 0 {
@@ -169,19 +115,11 @@ func main() {
 	file, _ := os.Open("4/input.txt")
 	input, _ := ReadInput(file)
 
-	var passports []Passport
-	for _, mp := range input {
-		if p, ok := ConvertMapToPassport(mp); ok {
-			passports = append(passports, p)
+	var count = 0
+	for _, element := range input {
+		if validation, _ := ValidatePassport(element); validation {
+			count++
 		}
 	}
-
-	var passportsWithValidValues []Passport
-	for _, p := range passports {
-		if ok := IsPassportValuesValid(p); ok {
-			passportsWithValidValues = append(passportsWithValidValues, p)
-		}
-	}
-
-	fmt.Println("part one", len(passports), "part two", len(passportsWithValidValues))
+	fmt.Println(count)
 }
